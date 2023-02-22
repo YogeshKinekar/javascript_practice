ee27714a5d63854022012cd1c79695c6d3991097
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-02-22 12:20:26 +0530

    added what is your age

---
+++ Conditionals_Expressions/practise.js
@@ -0,0 +1,9 @@
+// ----What is your age?-----
+
+let age=25;
+if(age>10 && age<20){
+    console.log("Your age lies between 10 and 20")
+}
+else {
+  console.log("Your age  doesnt lies between 10 and 20")
+}
