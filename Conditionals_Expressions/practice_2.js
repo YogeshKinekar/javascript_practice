387717778774705a074e5ceffeb7b25903da15ab
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-02-22 19:28:46 +0530

    added operators and conditionals

---
+++ Conditionals_Expressions/practice_2.js
@@ -0,0 +1,18 @@
+// What is your age solution in switch case 
+
+let age = 26;
+switch (age){
+    case 12: console.log("Your age is 12");
+    case 13: console.log("Your age is 13");
+    case 14: console.log("Your age is 14");
+    case 15: console.log("Your age is 15");
+    default: console.log("Your age is not special")
+}
+
+
+
+
+
+
+
+
