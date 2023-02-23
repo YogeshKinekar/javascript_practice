596b3b3d4a0cc3c37f757b9e5a86f2044301e754
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-02-23 19:13:01 +0530

    array:added arrays

---
+++ arrays/index.js
@@ -0,0 +1,12 @@
+let marks_class_12 =[81, 92, 73, 64, 75, 86] 
+console.log(marks_class_12)
+console.log(marks_class_12[0])
+console.log(marks_class_12[2])
+console.log(marks_class_12[3])
+console.log(marks_class_12[4])
+console.log(marks_class_12[5])
+console.log(marks_class_12[6]) // Will be undefined because index 6 does not exist
+console.log("The length of marks-class_12 is", marks_class_12.length)
+marks_class_12[6] = 89 // Adding a new value to the array
+marks_class_12[0] = 96 // Changing the value of an array
+console.log(marks_class_12)
