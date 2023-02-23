630438c8657f753e6dc3f1e44edc4df8c779506f
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-02-23 17:59:08 +0530

    added string methods

---
+++ string_methods/index.js
@@ -0,0 +1,26 @@
+// Print the length of string 
+
+
+let name = "Harry\""
+console.log(name.length);
+
+// Print the string to uppercase 
+
+let name1 = "Harry"
+console.log(name1.toUpperCase());
+
+
+// Print the string to LowerCase
+
+let name2 = "Harry"
+console.log(name2.toLowerCase());
+
+// Print the string to slice (2, 4)
+
+let name3 = "Harry"
+console.log(name3.slice(2, 4));
+
+// Print the string to slice (2)
+
+let name4 = "Harry"
+console.log(name4.slice(2));
---
+++ string_methods/index_1.js
@@ -0,0 +1,16 @@
+// Print the string in replace method 
+
+let name = "Yogesh"
+console.log(name.replace("Yo", "Na"));
+
+
+// Print the string in concat method
+
+let friend = "Naman"
+console.log(name.concat(" is a friend of ", friend, " ok "));
+
+
+// Print the string in trim method  
+
+let name2 = "       Meena         "
+console.log(name2.trim());
