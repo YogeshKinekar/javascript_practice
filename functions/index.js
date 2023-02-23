1dec84a3384e06741a62c749e58c905379a87dda
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-02-23 11:15:45 +0530

    added functions

---
+++ functions/index.js
@@ -0,0 +1,12 @@
+// Write a function to print average of given values
+
+function onePlusAvg(x,y){
+    return 1 + (x + y)/2
+}
+let a = 1;
+let b = 2;
+let c = 3;
+
+console.log(onePlusAvg(a,b));
+console.log(onePlusAvg(b,c));
+console.log(onePlusAvg(a,c));
---
+++ functions/index_1.js
@@ -0,0 +1,15 @@
+// Write a arrow function to print the addition of two numbers.
+
+const sum = (p,q)=>{
+    return p + q ;
+}
+console.log(sum(9,7));
+
+
+// Write a arrow function to print hi.
+const hello = ()=>{
+    console.log("hey how are you. I am toh fine yaar");
+    return "hi";
+}
+let v = hello();
+console.log(v);
