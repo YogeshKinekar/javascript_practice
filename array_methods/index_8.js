4299d33ad796586a7499008be0c90ec6a93716ce
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-02-25 11:46:29 +0530

    added: array methods

---
+++ array_methods/index_8.js
@@ -0,0 +1,9 @@
+// sort method 
+
+
+let compare = (a, b)=>{
+    return a - b ;
+}
+let num = [1, 22, 35, 14, 5, 444, 56, 84, 6]
+num.sort(compare);
+console.log(num);
---
+++ array_methods/index_9.js
@@ -0,0 +1,13 @@
+// splice method 
+  
+
+let num = [1, 22, 35, 14, 5, 444, 56, 84, 6]
+num.splice(2,4,120,1023,1526,546)
+console.log(num);
+
+// slice method
+
+
+let num1 = [1, 22, 35, 14, 5, 444, 56, 84, 6]
+let newNum = num1.slice(3,5);
+console.log(newNum);
