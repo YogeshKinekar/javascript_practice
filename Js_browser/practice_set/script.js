13258d52f5d5003c2973ee85888808f5bcd77338
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-03-11 16:41:10 +0530

    added prompt to change the background color as per the user input

--- Js_browser/practice_set/script.js
+++ Js_browser/practice_set/script.js
@@ -57,9 +57,13 @@
 // 4.Write a program to change the url to google.com if user enters a number greater than 4
 
 
-let number = prompt("Enter your number")
-number = Number.parseInt(number)
+// let number = prompt("Enter your number")
+// number = Number.parseInt(number)
 
-if (number > 4) {
-  location.href = "https://google.com"
-}
+// if (number > 4) {
+//   location.href = "https://google.com"
+// }
+
+// 5.Change the background of the page to yellow,red or any other color based on the user input through prompt 
+let color = prompt("Enter the page background color")
+document.body.style.background = color 
