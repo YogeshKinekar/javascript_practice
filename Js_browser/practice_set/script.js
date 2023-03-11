e425ec58b0686f78165e181c2bbf2c6e92ef9cce
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-03-11 12:23:18 +0530

    added prompt function to check the user can drive or not

---
+++ Js_browser/practice_set/script.js
@@ -0,0 +1,13 @@
+//  write a program using prompt function to take input of age as a value from the user and use a text to tell him if he can drive ? 
+let age = prompt("Enter your age")
+age = Number.parseInt(age)
+
+const canDrive = (age)=>{
+    age>=18?true:false;
+}
+if(canDrive(age)){
+    alert("Yes you can drive")
+}
+else{
+    alert("You cannot drive")
+}
