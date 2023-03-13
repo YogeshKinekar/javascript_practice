c1b33e0b0c0f4aaf356f557895a2ff96fef1f565
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-03-13 12:19:03 +0530

    added queryselector getelementsbytagname getelementsbyname for searching elements

--- walking_the_dom/searching_dom/script.js
+++ walking_the_dom/searching_dom/script.js
@@ -8,3 +8,9 @@ ctitles[0].style.color = "blue"
 ctitles[1].style.color = "red"
 ctitles[2].style.color = "green"
 console.log(ctitles)
+
+document.querySelector(".this").style.color = "cyan"
+document.querySelector(".this").style.background = "green"
+
+console.log(document.getElementsByTagName("a"))
+console.log(document.getElementsByName("search"))
