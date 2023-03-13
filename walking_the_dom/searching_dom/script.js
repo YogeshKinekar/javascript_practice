32031afac84ea1f199b843ac111de80fb8353728
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-03-13 11:55:27 +0530

    added queryselectorall for multiple style effect

--- walking_the_dom/searching_dom/script.js
+++ walking_the_dom/searching_dom/script.js
@@ -3,3 +3,8 @@ let ctitle = document.getElementById("firstcardtitle")
 ctitle.style.color ="red"
 
 
+let ctitles = document.querySelectorAll(".card-title")
+ctitles[0].style.color = "blue"
+ctitles[1].style.color = "red"
+ctitles[2].style.color = "green"
+console.log(ctitles)
