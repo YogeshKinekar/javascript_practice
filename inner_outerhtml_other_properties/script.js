5f7dfde8f78e9e0a2d81be30ace9e4c0c782ca91
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-03-15 11:58:57 +0530

    added element to see in body first child and first element child node name

--- inner_outerhtml_other_properties/script.js
+++ inner_outerhtml_other_properties/script.js
@@ -1,4 +1,7 @@
-let x = document.getElementsByTagName('span')[0]
-console.log(x)
-let y = document.getElementsByTagName('span')[0]
-console.dir(y)
+// let x = document.getElementsByTagName('span')[0]
+// console.log(x)
+// let y = document.getElementsByTagName('span')[0]
+// console.dir(y)
+console.log(document.body.firstChild.nodeName) 
+console.log(document.body.firstElementChild.nodeName)
+
