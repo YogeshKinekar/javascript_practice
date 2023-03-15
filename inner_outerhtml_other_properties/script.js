aeeb53ae74285bb93b4e3d0d546cf9682f47d43c
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-03-15 12:00:30 +0530

    added a first inner html

--- inner_outerhtml_other_properties/script.js
+++ inner_outerhtml_other_properties/script.js
@@ -2,6 +2,11 @@
 // console.log(x)
 // let y = document.getElementsByTagName('span')[0]
 // console.dir(y)
-console.log(document.body.firstChild.nodeName) 
-console.log(document.body.firstElementChild.nodeName)
+
+// console.log(document.body.firstChild.nodeName) 
+// console.log(document.body.firstElementChild.nodeName)
+
+first.innerHTML 
+first.innerHTML = "<i>hey I am italic</i>" 
+
 
