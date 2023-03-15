38e975004aabf74f523859c1c8fa664ef3d0f1a3
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-03-15 12:06:36 +0530

    added outer html in script

--- inner_outerhtml_other_properties/script.js
+++ inner_outerhtml_other_properties/script.js
@@ -6,7 +6,12 @@
 // console.log(document.body.firstChild.nodeName) 
 // console.log(document.body.firstElementChild.nodeName)
 
-first.innerHTML 
-first.innerHTML = "<i>hey I am italic</i>" 
+// first.innerHTML 
+// first.innerHTML = "<i>hey I am italic</i>" 
 
+first.outerHTML 
+first.outerHTML = "<div>hey</div>"
 
+// document.body.firstChild.data 
+// console.log(document.body.textContent)
+// first.hidden = false
