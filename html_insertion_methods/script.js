6765ba88dd807ca4dcb21c5f59ab8f9ffc148f2e
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-03-16 16:07:06 +0530

    added create element to add multiple elements by using javascript

--- html_insertion_methods/script.js
+++ html_insertion_methods/script.js
@@ -1,2 +1,6 @@
 let a = document.getElementsByTagName('div')[0]
-a.innerHTML = a.innerHTML + '<h1>Hello World</h1>';
+// a.innerHTML = a.innerHTML + '<h1>Hello World</h1>';
+
+let div = document.createElement("div");
+div.innerHTML = '<h1>Hello World</h1>';
+a.appendChild(div);
