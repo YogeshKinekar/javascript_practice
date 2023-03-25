fe1e1afbf84aefc5e999c75bcc8c19097d9b76a5
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-03-25 16:56:49 +0530

    added again the bookmarks using event listeners

---
+++ handling_browser_events/practice_set/script.js
@@ -0,0 +1,13 @@
+document.getElementById("google").addEventListener("click", function (){
+    window.location = "https://www.google.com";
+    win.focus();
+})
+
+document.getElementById("fb").addEventListener("click", function (){
+    window.location = "https://www.fb.com";
+    win.focus();
+})
+document.getElementById("twitter").addEventListener("click", function (){
+    window.location = "https://www.twitter.com";
+    win.focus();
+})
