a7624f48a158b5af664378ec48487dbf08f25c61
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-03-26 16:54:41 +0530

    added callbacks in program

--- introduction_to_callbacks/script.js
+++ introduction_to_callbacks/script.js
@@ -6,8 +6,27 @@
 
 
 // Asynchronous programming
-console.log("Start")
-setTimeout(function() {
-    console.log("Hey I am good");
-},3000)
-console.log("End")
+// console.log("Start")
+// setTimeout(function() {
+//     console.log("Hey I am good");
+// },3000)
+// console.log("End")
+
+// Callbacks
+function loadScript(src, callback) {
+    var script = document.createElement("script");
+    script.src = src;
+    script.onload = function () {
+        console.log("Loaded script with SRC: " + src)
+        callback(src);
+    }
+    document.body.appendChild(script);
+}
+function hello(src) {
+    alert("Hello World!" + src);
+}
+
+function goodmorning(src) {
+    alert("goodmorning" + src);
+}
+loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js", hello)
