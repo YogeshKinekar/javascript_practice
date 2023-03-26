15540edb1630397b76c4ef211c21444af05d69e4
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-03-26 17:43:35 +0530

    added callbacks again for error finding

--- introduction_to_callbacks/script.js
+++ introduction_to_callbacks/script.js
@@ -13,20 +13,56 @@
 // console.log("End")
 
 // Callbacks
+// function loadScript(src, callback) {
+//     var script = document.createElement("script");
+//     script.src = src;
+//     script.onload = function () {
+//         console.log("Loaded script with SRC:" + src)
+//         callback(src);
+//     }
+//     document.body.appendChild(script);
+// }
+// function hello(src) {
+//     alert("Hello World!" + src);
+// }
+
+// function goodmorning(src) {
+//     alert("goodmorning" + src);
+// }
+// loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js", hello)
+
+
 function loadScript(src, callback) {
     var script = document.createElement("script");
     script.src = src;
-    script.onload = function () {
-        console.log("Loaded script with SRC: " + src)
-        callback(src);
+    script.onload = function() {
+      console.log("Loaded script with SRC: " + src)
+      callback(null, src);
+    }
+    script.onerror = function() {
+      console.log("Error loading script with SRC: " + src);
+      callback(new Error("Src got some error"))
     }
     document.body.appendChild(script);
-}
-function hello(src) {
-    alert("Hello World!" + src);
-}
-
-function goodmorning(src) {
-    alert("goodmorning" + src);
-}
-loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js", hello)
+  }
+  
+  function hello(error, src) {
+    if (error) {
+      console.log(error)
+      return
+    }
+    alert('Hello World!' + src);
+  }
+  
+  
+  function goodmorning(error, src) {
+    
+    if (error) {
+      console.log(error)
+      sendEmergencyMessageToCeo();
+      return
+    }
+    alert('Good morning' + src);
+  }
+  
+  loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js", goodmorning) 
