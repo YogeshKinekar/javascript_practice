847f01779205470c03b144d42a2cc6d857b99a2a
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-04-01 06:18:37 +0530

    added comparison operators in javascript

---
+++ Js_operators/index_1.js
@@ -0,0 +1,22 @@
+// Comparison operators in JavaScript 
+
+
+// greater than operator 
+const a = 3, b = 2;
+console.log(a > b);  
+
+// equal operator
+console.log(2 == 2); 
+console.log(2 == '2'); 
+
+// not equal operator
+console.log(3 != 2); 
+console.log('hello' != 'Hello'); 
+
+// strict equal operator
+console.log(2 === 2);
+console.log(2 === '2'); 
+
+// strict not equal operator
+console.log(2 !== '2');
+console.log(2 !== 2); 
