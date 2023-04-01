6d63f55dd5166625e0fa84b1739df36b40067781
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-04-01 06:45:32 +0530

    added string operators that joins two or more string in javascript

---
+++ Js_operators/index_5.js
@@ -0,0 +1,9 @@
+// String operators in JavaScript 
+
+// concatenation operator
+console.log('hello' + 'world');
+
+let a = 'JavaScript';
+
+a += ' tutorial';  // a = a + ' tutorial';
+console.log(a);
