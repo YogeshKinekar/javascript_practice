fd93ea34cd402f356cc2bc1a9cfed7f7c226b60b
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-04-01 07:19:27 +0530

    added a program showing local scope of a variable

---
+++ functions/Js_variable_scope/index.js
@@ -0,0 +1,12 @@
+// Local Scope Variable
+
+// program showing local scope of a variable
+let a = "hello";
+
+function greet() {
+    let b = "World"
+    console.log(a + b);
+}
+
+greet();
+console.log(a + b); // error 
