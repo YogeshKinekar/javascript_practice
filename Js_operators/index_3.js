9f8e6f4b1d9f5711e7bd091686aab3330e2ad298
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-04-01 06:33:08 +0530

    added javascript ternary operator

---
+++ Js_operators/index_3.js
@@ -0,0 +1,10 @@
+// JavaScript Ternary Operator 
+
+// program to check pass or fail
+
+let marks = prompt('Enter your marks :');
+
+// check the condition
+let result = (marks >= 40) ? 'pass' : 'fail';
+
+console.log(`You ${result} the exam.`);
