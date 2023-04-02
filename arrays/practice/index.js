b17601e6f67c33fdc44edc3520db739300ed81df
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-04-02 06:54:13 +0530

    added program using push and unshift method oo the array

---
+++ arrays/practice/index.js
@@ -0,0 +1,23 @@
+// You can use the built-in method push() and unshift() to add elements to an array. 
+
+// The push() method adds an element at the end of the array 
+
+
+let gymStudents = ['shubham', 'sohit', 'rahul'];
+ 
+// add an element at the end 
+gymStudents.push('yogesh');
+
+console.log(gymStudents);
+
+
+
+// The unshift() method adds an element at the beginning of the array 
+
+let dailyActivities = ['eat', 'sleep'];
+
+//add an element at the start
+dailyActivities.unshift('work'); 
+
+console.log(dailyActivities); // ['work', 'eat', 'sleep']
+
