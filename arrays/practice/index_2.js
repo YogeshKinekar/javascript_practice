955a1f59ea45c62dec15a1ffd88c4c9fb168ebe4
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-04-02 07:22:46 +0530

    added pop method to remove last element from array

---
+++ arrays/practice/index_2.js
@@ -0,0 +1,14 @@
+// You can use the pop() method to remove the last element from an array. The pop() method also returns the returned value 
+
+let myCars = ['Brezza', 'Baleno', 'Harrier', 'Swift'];
+
+myCars.pop();
+console.log(myCars);
+
+
+
+
+const removedElements = myCars.pop()
+
+console.log(removedElements);
+console.log(myCars);
