1adc4df60c491ebb970d56fb67edf96859d92033
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-04-02 07:12:03 +0530

    added program add elements or change the elements by accessing the index value

---
+++ arrays/practice/index_1.js
@@ -0,0 +1,9 @@
+// You can also add elements or change the elements by accessing the index value
+
+let dailyActivities = [ 'eat', 'sleep', 'dancing'];
+
+// this will add the new element 'exercise' at the 2 index
+dailyActivities[3] = 'exercise';
+
+console.log(dailyActivities); // ['eat', 'sleep', 'exercise']
+
