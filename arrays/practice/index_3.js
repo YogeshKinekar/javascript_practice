3ab0cc748d8bf4a395df9987d94ec63da8247ac4
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-04-02 07:28:15 +0530

    added shift method to remove the first element

---
+++ arrays/practice/index_3.js
@@ -0,0 +1,8 @@
+// If you need to remove the first element, you can use the shift() method. 
+
+let myName = ['Yogesh', 'Prakash', 'Kinekar'];
+
+// removes the first element 
+
+myName.shift();
+console.log(myName);
