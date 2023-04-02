3a685d198cc174ebb1c3f5ca3654a94e45ad6ab4
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-04-02 06:07:01 +0530

    added break with for loop

---
+++ for_loops/index_8.js
@@ -0,0 +1,12 @@
+// Break with for loop 
+
+// program to print the value of i
+let i = 0;
+
+for(let i = 1; i <= 5; i++){
+    if(i == 3){
+        // break condition
+        break;
+    }
+    console.log(i);
+}
