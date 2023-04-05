30096ea90bcb38cbfed46d1ec69ec7f836dab4af
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-04-05 21:51:32 +0530

    added function to convert Celsius to fahrenheit

---
+++ functions/practice/index_4.js
@@ -0,0 +1,9 @@
+// Write a function to convert Celsius to Fahrenheit 
+
+function calFahrenheit(cel) {
+    return (cel*9/5) +32;
+}
+
+console.log(calFahrenheit(0));
+console.log(calFahrenheit(3));
+console.log(calFahrenheit(5));
