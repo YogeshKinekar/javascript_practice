dd6a65aa62680416c2c60586a243b263f69f33d0
yogeshkinekar <121310142+YogeshKinekar@users.noreply.github.com>
2023-05-19 23:27:59 +0530

    Update index_3.js
    Added Math.exp()

--- Js_math/index_3.js
+++ Js_math/index_3.js
@@ -5,3 +5,20 @@ console.log(num);
 // floor with negative number
 var num1 = Math.floor(-0.456);
 console.log(num1);
+
+
+// JavaScript Math.exp()
+// calculate e raised to the power of 1
+var value1 = Math.exp(1);
+
+console.log(value1); 
+
+// calculate e raised to the power of 2
+var value2 = Math.exp(2);
+
+console.log(value2); 
+
+// calculate e raised to the power of 5
+var value3 = Math.exp(5);
+
+console.log(value3); 
