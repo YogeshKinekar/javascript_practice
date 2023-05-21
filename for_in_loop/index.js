299b775edf0ae7f67ae607a9d7ccca51480247da
yogeshkinekar <121310142+YogeshKinekar@users.noreply.github.com>
2023-05-21 22:52:35 +0530

    Update index.js
    Added for in array

--- for_in_loop/index.js
+++ for_in_loop/index.js
@@ -10,3 +10,17 @@ for ( let key in student ) {
     // display the properties
     console.log(`${key} => ${student[key]}`);
 }
+
+
+
+// for...in with Arrays
+// You can also use for...in with arrays
+
+
+// define array
+const arr = [ 'hello', 1, 'JavaScript' ];
+
+// using for...in loop
+for (let x in arr) {
+    console.log(arr[x]);
+}
