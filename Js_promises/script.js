24abe015530195edae677b30dd174e4c2593638a
yogeshkinekar <121310142+YogeshKinekar@users.noreply.github.com>
2023-05-22 22:27:19 +0530

    Update script.js
    Added finally() method

--- Js_promises/script.js
+++ Js_promises/script.js
@@ -11,3 +11,22 @@ let countValue = new Promise(function (resolve, reject) {
 });
 
 console.log(countValue);
+
+
+
+// JavaScript finally() method
+// You can also use the finally() method with promises. The finally() method gets executed when the promise is either resolved successfully or rejected.
+
+
+// returns a promise
+let countValue = new Promise(function (resolve, reject) {
+    // could be resolved or rejected   
+    resolve('Promise resolved'); 
+});
+
+// add other blocks of code
+countValue.finally(
+    function greet() {
+        console.log('This code is executed.');
+    }
+);
