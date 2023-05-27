4d406210b9e6d9ebce1b8bacb410a3ba7d5b30b1
yogeshkinekar <121310142+YogeshKinekar@users.noreply.github.com>
2023-05-27 23:32:19 +0530

    Added find the second largest element from the array by using Bubble sort

---
+++ practice_digikull/new2/script_2.js
@@ -0,0 +1,28 @@
+// find the second largest element from the array by using Bubble sort
+
+// example 
+
+// input : arr = [1,0,9,6,8,3,10]
+
+// Output : 9
+
+function bubble_sort (arr) {
+    //Write your code here  
+       for (let i = 0; i < arr.length; i++){
+         let flag = true;
+         for(let j = 0; j < arr.length -1; j++){
+         if(arr[j] > arr[j+1]){
+           flag = false;
+           let temp = arr[j];
+           arr[j] = arr[j+1];
+           arr[j+1] = temp;
+         }
+       }
+       if(flag){
+         break;
+       }
+     }
+    return arr[arr.length -2];
+    };
+    console.log(bubble_sort([1,0,9,6,8,3,10]))
+    module.exports = {bubble_sort};
