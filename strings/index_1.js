6a2fc144b07a8dd3b1aaa5ce558aec1c1d189eff
yogeshkinekar <121310142+YogeshKinekar@users.noreply.github.com>
2023-06-08 22:30:05 +0530

    Update index_1.js

--- strings/index_1.js
+++ strings/index_1.js
@@ -5,3 +5,24 @@ let boy2 = "Nikhil";
 
 let sentence = `${boy1} is a friend of ${boy2}`;
 console.log(sentence);
+
+
+
+
+// program to sort words in alphabetical order
+
+// take input
+const string = prompt('Enter a sentence: ');
+
+// converting to an array
+const words = string.split(' ');
+
+// sort the array elements
+words.sort();
+
+// display the sorted words
+console.log('The sorted words are:');
+
+for (const element of words) {
+  console.log(element);
+}
